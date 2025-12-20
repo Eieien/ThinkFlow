39e9eb7146031dd11843d40ffb9a0d78a13c8115
vango98 <greencolorapple@gmail.com>
2025-12-20 15:03:36 +0800

    server updated

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.ALLOWED_ORIGIN,
     credentials: true
 }));
 // app.use(limiter);
