bea57884d25b4e8eb9dac2254d37e86e28bc2f85
vango98 <greencolorapple@gmail.com>
2025-12-20 17:54:05 +0800

    fwregf

--- src/api/axiosInstances.ts
+++ src/api/axiosInstances.ts
@@ -1,10 +1,10 @@
 import axios, { type AxiosInstance } from 'axios';
 
 const axiosPublic : AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'http://localhost:20155/api'
 });
 export const axiosPrivate : AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'http://localhost:20155/api'
 });
 
 export default axiosPublic;
